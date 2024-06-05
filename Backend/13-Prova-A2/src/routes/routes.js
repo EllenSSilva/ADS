2f624daece8b9cac14d835e56b4d5e3f6aa8a7e0
Ellen Silva <137661449+EllenSSilva@users.noreply.github.com>
2024-06-05 20:44:32 -0300

    projeto

--- Backend/13-Prova-A2/src/routes/routes.js
+++ Backend/13-Prova-A2/src/routes/routes.js
@@ -1,11 +1,36 @@
 const express = require('express')
 const router = express.Router()
 
-// controllers
+// Controllers
 const CargoController = require('../controller/CargoController')
 const DepartamentoController = require('../controllers/DepartamentoController')
 const FuncionarioController = require('../controllers/FuncionarioController')
 const ProjetoController = require('../controllers/ProjetoController')
 const TarefaController = require('../controllers/TarefaCon  troller')
 
-// validators
+// Validators
+const {validarID} = require ('../validators/Idvalidator')
+const {validarCargo} = require ('../validators/CargoValidator')
+const {validarDepartamento} = require ('../validators/DepartamentoValidator')
+const {validarFuncionario} = require ('../validators/FuncionarioValidator')
+const {validarProjeto} = require ('../validators/ProjetoValidator')
+const {validarTarefa} = require ('../validators/TarefaValidator')
+
+// Abaixo encontram-se as areas do projeto
+
+// Agendamento 
+
+// Serviço 
+
+// Funcionario
+
+// Departamento
+
+// Cargo
+
+// pet
+
+// cliente 
+
+
+
