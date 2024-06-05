4ff3bfc00babf12bcf929464a0fd0966ad8013bc
Ellen Silva <137661449+EllenSSilva@users.noreply.github.com>
2024-06-05 20:14:32 -0300

    projeto

--- Backend/13-Prova-A2/src/routes/routes.js
+++ Backend/13-Prova-A2/src/routes/routes.js
@@ -2,4 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 // controllers
-const CargoController = require('.../controlle')
+const CargoController = require('../controller/CargoController')
+const DepartamentoController = require('../controllers/DepartamentoController')
+const FuncionarioController = require('../controllers/FuncionarioController')
+const ProjetoController = require('../controllers/ProjetoController')
+const TarefaController = require('../controllers/TarefaCon  troller')
+
+// validators
